b1cced561920f3dfda82fcba33b23f7b62fdb82e
fehervendel <fehervendel@gmail.com>
2023-10-11 15:55:20 +0200

    bugfix: restore identityMigration

---
+++ SolarWatch/SolarWatch/Migrations/User/20231011135345_initialMigrationIdentity.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SolarWatch.Migrations.User
+{
+    public partial class initialMigrationIdentity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
